b25abb9ce4bcc225f66e92c35227e03dabb498fd
augustin <agu.clessin@gmail.com>
2021-12-15 14:57:00 +0000

    RAPToT avec les plots

--- src/RAPToR.R
+++ src/RAPToR.R
@@ -8,6 +8,8 @@ tx2gn <- read.table("data/tx2gn.csv", h=T, sep=',', as.is=T)
 data=tximport(fullpaths,'salmon', tx2gene = tx2gn)#tx2gene c'est pour transformer les noms de transcripts en noms de gènes
 colnames(data$abundance)=c("alg-1 (rep1)","alg-1(rep2)","alg-1 (rep3)","WT (rep1)","WT (rep2)","WT (rep3)")
 
+upgenes=read.table("results/fGOup.data",h=F,sep='\n',as.is=T)$V1
+downgenes=read.table("results/fGOdown.data",h=F,sep='\n',as.is=T)$V1
 colData=data.frame(paths,factor(c("Alg1","Alg1","Alg1","WT","WT","WT"),levels = c("WT", "Alg1")))
 colnames(colData)=c("sample","treatment")
 data$normlog=normalizeBetweenArrays(data$abundance,method = "quantile")
@@ -35,5 +37,17 @@ refCompare <-function(samp, ref, ae_obj, fac){
   return(list(samp=lm_samp, ref=lm_ref, ovl_genelist=ovl$inter.genes))
 }
 
-comparaison=refCompare(data$abundance,ref,pseudoage_data,colData$treatment)
-plot(comparaison)
+comparaison=refCompare(log1p(data$abundance),ref,pseudoage_data,colData$treatment)
+par(pty="s")
+#colors = 2*(comparaison$ovl_genelist %in% upgenes + 0) + (comparaison$ovl_genelist %in% downgenes)+1# ça sert à rien 
+
+plot(comparaison$ref$coefficients[2,],comparaison$samp$coefficients[2,],
+     ylim=c(-5,5),xlim=c(-5,5),col=1,cex=0.4,xlab='log2fold ref',ylab='log2fold sample',main="alg-1")
+
+points(comparaison$ref$coefficients[2, comparaison$ovl_genelist %in% upgenes],
+       comparaison$samp$coefficients[2,comparaison$ovl_genelist %in% upgenes],col=2,cex=0.8)
+points(comparaison$ref$coefficients[2, comparaison$ovl_genelist %in% downgenes],
+       comparaison$samp$coefficients[2,comparaison$ovl_genelist %in% downgenes],col=3,cex=0.8)
+#abline(0,cor(comparaison$ref$coefficients[2,],comparaison$samp$coefficients[2,]),col=4,cex=5)
+legend('bottomright',legend=c("upregulated","downregulated","not significant"),
+       col=c(3,2,1), pch=1, lwd=2, lty=NA,x.intersp = 0.05, bty="n")
