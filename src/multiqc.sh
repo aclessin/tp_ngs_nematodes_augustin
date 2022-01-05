38774c3c69895ee72a31dc4c21ca57c5b71bc58d
augustin <agu.clessin@gmail.com>
2022-01-05 23:16:52 +0000

    extra comment

--- src/multiqc.sh
+++ src/multiqc.sh
@@ -2,6 +2,5 @@
 
 
 
-cd ~/mydatalocal/tp_ngs_nematodes_augustin/results/fastqc_reports_after_trimmomatic
-
+cd ~/mydatalocal/tp_ngs_nematodes_augustin/results/fastqc_reports_after_trimmomatic "put the path where you have the fastqc reports"
 multiqc .
